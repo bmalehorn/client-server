251a2616b34170ba5853c1ca5ca5c8be499f7f46
Brian Malehorn <bmalehorn@gmail.com>
2016-12-17 10:53:50 -0800

    printf() on errors

--- client.c
+++ client.c
@@ -9,7 +9,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-void main(void) {
+int main(void) {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in srv;
     memset(&srv, 0, sizeof(srv));                  // clear it first
@@ -17,15 +17,22 @@ void main(void) {
     srv.sin_addr.s_addr = inet_addr("127.0.0.1");  // IP address of server
     srv.sin_port = htons(15440);                   // server port
     int rv = connect(sockfd, (struct sockaddr *)&srv, sizeof(struct sockaddr));
-    assert(rv >= 0);
+    if (rv < 0) {
+        perror("connect");
+        return 1;
+    }
     int flag = 1;
     setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, (char *)&flag, sizeof(int));
 
     while (1) {
-        printf("sending?\n");
         char *msg = "hello";
-        rv = send(sockfd, msg, strlen(msg) + 1 /* include '\0' */, 0);
-        printf("sent? rv = %d\n", rv);
+        int n = strlen(msg);
+        printf("%s: sending msg = \"%s\", n = %d\n", __FILE__, msg, n);
+        rv = send(sockfd, msg, n, 0);
+        if (rv <= 0) {
+            perror("send");
+            return 1;
+        }
         sleep(1);
     }
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 #include <netinet/tcp.h>
 #include <assert.h>
 
-void main(void) {
+int main(void) {
     // server
     int port = 15440;
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);  // TCP/IP socket
@@ -16,24 +16,34 @@ void main(void) {
     srv.sin_addr.s_addr = htonl(INADDR_ANY);  // don't care IP address
     srv.sin_port = htons(port);               // server port
     int rv = bind(sockfd, (struct sockaddr *)&srv, sizeof(struct sockaddr));
-    assert(rv >= 0);
+    if (rv < 0) {
+        perror("bind");
+        return 1;
+    }
+
     int flag = 1;
     setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, (char *)&flag, sizeof(int));
 
     rv = listen(sockfd, 5);
-    assert(rv >= 0);
+    if (rv < 0) {
+        perror("listen");
+        return 1;
+    }
     while (1) {
-        printf("accepting\n");
+        printf("%s: accepting\n", __FILE__);
+
         int sa_size = sizeof(struct sockaddr_in);
         int sessfd = accept(sockfd, (struct sockaddr *)&cli, &sa_size);
         while (1) {
             char buf[100];
             rv = recv(sessfd, buf, sizeof(buf), 0);
-            if (rv == 0) {
-                printf("bad recv(), restarting\n");
+            if (rv <= 0) {
+                printf("%s: bad recv(), restarting\n", __FILE__);
                 break;
+            } else {
+                buf[rv] = '\0';
             }
-            printf("buf = %s\n", buf);
+            printf("%s: received msg = \"%s\", n = %d\n", __FILE__, buf, rv);
         }
     }
 }
